446eb798c3e4d50a922548b8b50353d6366b3142
Lucas Torres <lucastorresgomes@gmail.com>
2024-06-24 09:16:41 -0300

    List of characters and Get by id

--- Controllers/CharacterController.cs
+++ Controllers/CharacterController.cs
@@ -1,17 +1,31 @@
+using dotnet_rpg.Models;
 using Microsoft.AspNetCore.Mvc;
 
 namespace dotnet_rpg.Controllers
 {
     [ApiController]
-    ///Route nao esta criando a rota
     [Route("api/[controller]")]
     public class CharacterController : ControllerBase
     {
-        private static Character knight = new Character();
+        /// List of characters
+        private static List<Character> characters = new List<Character> {
+            new Character(),
+            new Character { Id = 1, Name = "Sam" }
+        };
 
-        public IActionResult Get()
+        /// Get all on the list
+        [HttpGet("GetAll")]
+        public ActionResult<List<Character>> Get()
         {
-            return Ok(knight);
+            return Ok(characters);
         }
+
+        /// Pegar por personagem por id 
+        [HttpGet("{id}")]
+        public ActionResult<List<Character>> GetSingle(int id)
+        {
+            return Ok(characters.FirstOrDefault(c => c.Id == id));
+        }
+        
     }
 }
--- Program.cs
+++ Program.cs
@@ -2,8 +2,7 @@ global using dotnet_rpg.Models;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
@@ -16,6 +15,7 @@ if (app.Environment.IsDevelopment())
     app.UseSwaggerUI();
 }
 
+app.MapControllers();
 app.UseHttpsRedirection();
 
 var summaries = new[]
